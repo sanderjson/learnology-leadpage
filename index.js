b88c49c6176f7e363688460990d2427ddac835fe
Jonathan Sanderson <sanderson.jma@gmail.com>
2020-04-21 10:46:35 -0600

    test

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-const encode = (data) => {
-	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
-};
+// const encode = (data) => {
+// 	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
+// };
 
 const processForm = (form) => {
 	const data = new FormData(form);
 	fetch('/', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-		body: encode({ 'form-name': 'lead-form', ...data })
+		body: encode({ 'form-name': 'lead-form', data })
 	})
 		.then(() => {
 			console.log('Form has been submitted!');
