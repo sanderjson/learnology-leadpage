182dcec356733babc2a3a8138e9ef810c42695df
Jonathan Sanderson <sanderson.jma@gmail.com>
2020-04-21 11:29:58 -0600

    form fix

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 
-// const encode = (data) => {
-// 	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
-// };
+const encode = (data) => {
+	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
+};
 
-const processForm = (form) => {
-  const data = new FormData(form);
-  data.append('username', 'Chris');
-  console.log(data)
+const processForm = (formData) => {
+  // const data = new FormData(form);
+  // console.log(data)
 	fetch('/', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-		body: { 'form-name': 'lead-form', ...data }
+		body: encode({ 'form-name': 'lead-form', ...dataData })
 	})
 		.then(() => {
 			console.log('Form has been submitted!');
@@ -22,14 +21,22 @@ const processForm = (form) => {
 };
 
 const leadForm = document.getElementById('lead-magnet');
-console.log(leadForm)
+// console.log(leadForm)
 if (leadForm) {
 	leadForm.addEventListener('submit', (e) => {
-		e.preventDefault();
-    processForm(leadForm);
-	});
+    e.preventDefault();
+    let formData = {
+      firstName: e.target.elements[0].value,
+      lastName: e.target.elements[1].value,
+      email: e.target.elements[2].value,
+      isParent: e.target.elements[3].checked? true: false,
+      isStudent: e.target.elements[5].checked? true: false,
+    }
+    // console.log(formData)
+    processForm(formData)
+  });
 }
-
+  
 const downloadFile = () => {
   	const a = document.createElement('a');
 	a.style.display = 'none';
